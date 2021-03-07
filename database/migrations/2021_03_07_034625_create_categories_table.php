d60e7bf988de75ffc4aaab66d0ee9aabc8647a52
Walter Laura Soto <waltels20219@gmail.com>
2021-03-06 23:56:20 -0400

    Modelo 2

--- database/migrations/2021_03_07_034625_create_categories_table.php
+++ database/migrations/2021_03_07_034625_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLevelsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateLevelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('levels', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
 
             $table->string('name');
-            
+
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateLevelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('levels');
+        Schema::dropIfExists('categories');
     }
 }
