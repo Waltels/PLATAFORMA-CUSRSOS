11c2fedef7fb98495016c91c09b5e0cba68c8204
Walter Laura Soto <waltels20219@gmail.com>
2021-03-10 00:30:29 -0400

    vista de cursos

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,11 +10,47 @@ class Course extends Model
     use HasFactory;
 
     protected $guarded = ['id', 'status'];
+    protected $withCount = ['students', 'reviews'];
     
 
     const BORRADOR = 1;
     const REVISION = 2;
     const PUBLICADO = 3;
+
+    public function getRatingAttribute(){
+
+
+
+        if($this->reviews_count){
+
+            return round($this->reviews->avg('rating'), 1); 
+        }else{
+            return 5;
+        }           
+    }
+
+    //Query scopes
+
+    public function scopeCategory($query, $category_id){
+        if($category_id){
+
+            return $query->where('category_id', $category_id);
+
+        }
+    }
+
+    public function scopeLevel($query, $level_id){
+        if($level_id){
+
+            return $query->where('level_id', $level_id);
+
+        }
+    }
+
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
 
     //Relacion uno a muchos
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('cursos', [CourseController::class, 'index'])->name('courses.index');
+
+Route::get('cursos/{course}', function($course){
+    return "Aqui se mostrara la informacion del curso";
+})->name('courses.show');
